425b147ee33210a91b4c3375e4a1d5c0184d6e96
Lars Bonus <lars.bonus@outlook.com>
2023-05-23 16:29:42 +0200

    fetch data from API

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
+import React, { useEffect, useState } from "react";
 import Card from "@/components/Card";
 import Header from "@/components/Header";
-import coinData from "@/db/db.json";
 
 export default function HomePage() {
+  const [cryptos, setCryptos] = useState([]);
+
+  useEffect(() => {
+    async function startFetching() {
+      const response = await fetch(
+        "https://api.polygon.io/v2/aggs/grouped/locale/global/market/crypto/2023-05-22?apiKey=tgmy3Uasr3HfP7yyZ6UmwslF9LiHoGjr"
+      );
+      const cryptoData = await response.json();
+      const cryptos = cryptoData.results;
+
+      setCryptos(cryptos);
+    }
+
+    startFetching();
+  }, []);
+
   return (
     <div>
       <Header />
       <ul>
-        {coinData.map((coin) => (
-          <Card key={coin.name} name={coin.name} price={coin.price} />
+        {cryptos.map((crypto) => (
+          <Card key={crypto.T} name={crypto.T} price={crypto.c} />
         ))}
       </ul>
     </div>
